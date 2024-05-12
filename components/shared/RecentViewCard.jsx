75456bd260315dc76a88aa2d5a15104d92de6f01
emondewan00 <dewanmohammademon@gmail.com>
2024-05-12 23:27:32 +0600

    resolve text and underline overlapping

--- components/shared/RecentViewCard.jsx
+++ components/shared/RecentViewCard.jsx
@@ -12,10 +12,10 @@ const RecentViewCard = ({ product }) => {
         height={150}
         alt={title}
       />
-      <div className="absolute top-0 left-0 w-full text-white bg-blue-400/40  group-hover:flex justify-center items-center group-hover:h-full transition-all ease-in-out duration-500 h-0 rounded">
+      <div className="absolute top-0 left-0 w-full text-white bg-blue-400/40  group-hover:flex justify-center items-center group-hover:h-full transition-all ease-in-out duration-500 h-0 rounded text-balance text-center">
         <Link
           href={`/product/${id}`}
-          className=" group-hover:block hidden  after:transition-all after:ease-linear after:duration-200 after:delay-100 after:[] after:w-0 after:h-1 after:bg-white after:rounded after:absolute after:bottom-[60px] after:left-11 hover:after:w-[68px] "
+          className=" group-hover:block hidden  after:transition-all after:ease-linear after:duration-200 after:delay-100 after:[] after:w-0 after:h-1 after:bg-white after:rounded after:absolute  after:left-11 hover:after:w-[70px] after:mt-6 "
         >
           {title}
         </Link>
