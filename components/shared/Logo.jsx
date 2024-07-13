54fa912cc9e772c4e7034263dbf12c2bc550005b
emondewan00 <dewanmohammademon@gmail.com>
2024-07-13 21:41:06 +0600

    redesign navbar

--- components/shared/Logo.jsx
+++ components/shared/Logo.jsx
@@ -3,17 +3,15 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div>
-      <Link href={"/"}>
-        <Image
-          priority
-          src={"/images/logo-white.png"}
-          width={100}
-          height={100}
-          alt="logo"
-        />
-      </Link>
-    </div>
+    <Link href={"/"}>
+      <Image
+        priority
+        src={"/images/logo-white.png"}
+        width={100}
+        height={100}
+        alt="logo"
+      />
+    </Link>
   );
 };
 
