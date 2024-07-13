b3a60560b5b35ccc0c4dad785bd1e72b787bc902
emondewan00 <dewanmohammademon@gmail.com>
2024-07-13 16:51:33 +0600

    refactor and add somoe model and utility function

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -2,22 +2,65 @@ import mongoose, { Schema, model } from "mongoose";
 
 const productSchema = new Schema(
   {
-    name: { type: String, required: true ,index: true},
-    image: [{ type: String, required: true }],
-    price: { type: Number, required: true },
-    discount_price: { type: Number, required: true },
-    reviewsNumber: { type: Number, required: true },
-    rating: { type: Number, required: true },
-    availability: { type: Boolean, required: true },
-    brand: { type: String, required: true },
-    category: { type: String, required: true, lowercase: true },
+    name: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    image: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    price: {
+      type: Number,
+      required: true,
+    },
+    discount_price: {
+      type: Number,
+      required: true,
+    },
+    reviewsNumber: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
     details: Schema.Types.Mixed,
-    description: { type: String, required: true },
-    size: { type: [String] },
-    colors: { type: [String] },
-    sku: { type: String, required: true },
-    soldCount: { type: Number, required: true },
-    quantities: { type: Number, required: true },
+    description: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: [String],
+    },
+    colors: {
+      type: [String],
+    },
+    sku: {
+      type: String,
+      required: true,
+    },
+    soldCount: {
+      type: Number,
+      required: true,
+    },
+    quantities: {
+      type: Number,
+      required: true,
+    },
   },
   { timestamps: true }
 );
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,10 +1,24 @@
 import mongoose, { Schema, model } from "mongoose";
 const userSchema = new Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    isAdmin: { type: Boolean, required: true, default: false },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
   { timestamps: true }
 );
