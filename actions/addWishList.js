76fa2a945760c256650da35f9644ad55193e527a
emondewan00 <dewanmohammademon@gmail.com>
2024-07-17 21:29:26 +0600

    wishlit button work fine

--- actions/addWishList.js
+++ actions/addWishList.js
@@ -1,6 +1,6 @@
 "use server";
-import connectMongo from "@/lib/connectDb";
-import Wishlist from "@/schema/wishlistModel";
+import connectMongo from "@/utils/connectDb";
+import Wishlist from "@/models/wishlistModel";
 import { Types } from "mongoose";
 import { revalidateTag } from "next/cache";
 
@@ -13,10 +13,10 @@ const addToWishList = async (data) => {
   });
   revalidateTag("wishlistLength");
   if (find) {
-    return { message: "Product already added to wishlist" };
+    return { message: "Product already added to wishlist", status: "success" };
   }
   const newWishList = await Wishlist.create(data);
-  return { message: "Wishlist added successfully" };
+  return { message: "Wishlist added successfully", status: "success" };
 };
 
 export default addToWishList;
--- components/shared/ProductHover.jsx
+++ components/shared/ProductHover.jsx
@@ -1,5 +1,6 @@
 "use client";
 import cartAction from "@/actions/cartAction";
+import addToWishList from "@/actions/addWishList";
 import { useSession } from "next-auth/react";
 import { FaRegHeart, FaSearch, FaShoppingCart } from "react-icons/fa";
 import { toast } from "react-toastify";
@@ -20,6 +21,20 @@ const ProductHover = ({ product_id, name }) => {
     }
     toast.error("Failed to add product to cart.");
   };
+
+  const wishlistSubmit = async (e) => {
+    e.preventDefault();
+    if (!data?.user) return toast.info("Please login first");
+    const result = await addToWishList({
+      user_id: data?.user?.id,
+      product_id: product_id,
+    });
+    if (result.status === "success") {
+      return toast.success(`Added ${name} to your wishlist.`);
+    }
+    toast.error("Failed to add product to wishlist.");
+  };
+
   return (
     <div className="absolute top-4 right-4 space-y-2 opacity-0 transition-opacity delay-100 duration-300 ease-linear group-hover:opacity-100">
       <form onSubmit={cartSubmit} className="cursor-pointer">
@@ -27,16 +42,19 @@ const ProductHover = ({ product_id, name }) => {
           type="submit"
           className="bg-white text-gray-400 hover:bg-blue-400 p-2 rounded-full hover:text-white transition-all duration-300 delay-150"
         >
-          <FaShoppingCart className="" />
+          <FaShoppingCart />
         </button>
       </form>
-      <form>
-        <div className="bg-white text-gray-400 hover:bg-blue-400 p-2 rounded-full hover:text-white transition-all duration-300 delay-150">
-          <FaRegHeart className="" />
-        </div>
+      <form onSubmit={wishlistSubmit}>
+        <button
+          type="submit"
+          className="bg-white text-gray-400 hover:bg-blue-400 p-2 rounded-full hover:text-white transition-all duration-300 delay-150"
+        >
+          <FaRegHeart />
+        </button>
       </form>
       <div className="bg-white text-gray-400 hover:bg-blue-400 p-2 rounded-full hover:text-white transition-all duration-300 delay-150">
-        <FaSearch className="" />
+        <FaSearch />
       </div>
     </div>
   );
