f9c94bf60de5b05fc08154b66a7813c3495b96fe
emondewan00 <dewanmohammademon@gmail.com>
2024-08-01 23:58:37 +0600

    working toggle side nav bar

--- components/shared/Logo.jsx
+++ components/shared/Logo.jsx
@@ -1,17 +1,32 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 
 const Logo = () => {
+  const openSidebar = () => {
+    const element = document.querySelector(".mobile-nav");
+    element.classList.add("open-sidebar");
+  };
   return (
-    <Link href={"/"}>
-      <Image
-        priority
-        src={"/images/logo-white.png"}
-        width={100}
-        height={100}
-        alt="logo"
-      />
-    </Link>
+    <div className="flex gap-x-2">
+      <div
+        className="space-y-1 *:w-6 *:h-1 *:rounded *:block *:bg-white"
+        onClick={openSidebar}
+      >
+        <span />
+        <span />
+        <span />
+      </div>
+      <Link href={"/"}>
+        <Image
+          priority
+          src={"/images/logo-white.png"}
+          width={100}
+          height={100}
+          alt="logo"
+        />
+      </Link>
+    </div>
   );
 };
 
