a832129d00295b5373db221117716e00da0993e8
emondewan00 <dewanmohammademon@gmail.com>
2024-08-25 15:34:44 +0600

    register form replace with react form hook

--- components/register/RegisterForm.jsx
+++ components/register/RegisterForm.jsx
@@ -1,21 +1,32 @@
-// "use client";
+"use client";
+import { useForm } from "react-hook-form";
 import registerAction from "@/actions/register";
 import Link from "next/link";
-// import { useFormState } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
-  //   const [state, formAction] = useFormState(registerAction, null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const router = useRouter();
+  const onSubmit = async (data) => {
+    const result = await registerAction(data);
+    if (result.success) {
+      toast.success(result.message);
+      router.push("/login");
+    } else {
+      toast.error(result.message);
+    }
+  };
 
   return (
     <div className="self-center p-4 text-slate-700">
       <h1 className="text-4xl font-bold text-center">Sign Up</h1>
-      {/* {!state.success && (
-        <h3 className=" my-4 p-2 rounded bg-rose-600">{state.message} </h3>
-      )}
-      {state.success && (
-        <h3 className=" my-4 p-2 rounded bg-green-600">{state.message} </h3>
-      )} */}
-      <form action={registerAction} className="space-y-4">
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
         <div>
           <label htmlFor="name" className="text-xl">
             Name <span className="text-red-600">*</span>
@@ -23,10 +34,13 @@ const RegisterForm = () => {
           <input
             type="text"
             id="name"
-            name="name"
-            className="w-full p-3  outline-blue-900 focus:outline duration-150 delay-100 ease-linear rounded mt-2"
+            {...register("name", { required: "Name is required" })}
+            className="w-full p-3 outline-blue-900 focus:outline duration-150 delay-100 ease-linear rounded mt-2"
             placeholder="John Doe"
           />
+          {errors.name && (
+            <p className="text-red-600 mt-2">{errors.name.message}</p>
+          )}
         </div>
         <div>
           <label htmlFor="email" className="text-xl">
@@ -35,10 +49,19 @@ const RegisterForm = () => {
           <input
             type="email"
             id="email"
-            name="email"
-            className="w-full p-3  outline-blue-900 focus:outline duration-150 delay-100 ease-linear rounded mt-2"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/i,
+                message: "Please enter a valid email address",
+              },
+            })}
+            className="w-full p-3 outline-blue-900 focus:outline duration-150 delay-100 ease-linear rounded mt-2"
             placeholder="example@gmail.com"
           />
+          {errors.email && (
+            <p className="text-red-600 mt-2">{errors.email.message}</p>
+          )}
         </div>
         <div>
           <label htmlFor="password" className="text-xl">
@@ -47,11 +70,19 @@ const RegisterForm = () => {
           <input
             type="password"
             id="password"
-            name="password"
-            minLength={6}
-            className="w-full p-3  outline-blue-900 focus:outline duration-150 delay-100 ease-linear rounded mt-2"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters long",
+              },
+            })}
+            className="w-full p-3 outline-blue-900 focus:outline duration-150 delay-100 ease-linear rounded mt-2"
             placeholder="xxxxxxxx"
           />
+          {errors.password && (
+            <p className="text-red-600 mt-2">{errors.password.message}</p>
+          )}
         </div>
         <div className="flex justify-between items-center">
           <button
@@ -62,7 +93,7 @@ const RegisterForm = () => {
           </button>
 
           <p className="">
-            Already have an account ?{" "}
+            Already have an account?{" "}
             <Link href={"/login"} className="text-blue-600 underline">
               Login
             </Link>
