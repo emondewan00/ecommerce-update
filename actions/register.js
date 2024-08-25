ef8a0ce830245859c4793fa4a4870818b3c47d4a
emondewan00 <dewanmohammademon@gmail.com>
2024-08-25 15:34:44 +0600

    register form replace with react form hook

--- actions/register.js
+++ actions/register.js
@@ -1,15 +1,14 @@
 "use server";
-import connectMongo from "@/utils/connectDb"; 
+import connectMongo from "@/utils/connectDb";
 import UserModel from "@/models/UserModel";
 import bcrypt from "bcrypt";
 import { isRedirectError } from "next/dist/client/components/redirect";
 import { redirect } from "next/navigation";
 
-const registerAction = async (formData) => {
+const registerAction = async (data) => {
   const saltRound = 10;
-  const name = formData.get("name");
-  const email = formData.get("email");
-  const password = formData.get("password");
+
+  const { name, email, password } = data;
   if (!email || !password) {
     return {
       success: false,
@@ -19,21 +18,28 @@ const registerAction = async (formData) => {
   const hashedPassword = await bcrypt.hash(password, saltRound);
   try {
     await connectMongo();
-    const user = await UserModel.create({ email, password: hashedPassword , name});
+    const user = await UserModel.create({
+      email,
+      password: hashedPassword,
+      name,
+    });
     if (!user) {
       return {
         success: false,
         message: "Invalid email or password",
       };
     }
-    redirect("/login");
+    return {
+      success: true,
+      message: "User registered successfully",
+    };
   } catch (error) {
     if (isRedirectError(error)) {
       throw error;
     }
     return {
       success: false,
-      message: error.message,
+      message: error.message || "Please try again !",
     };
   }
 };
