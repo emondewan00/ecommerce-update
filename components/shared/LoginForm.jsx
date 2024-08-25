61c5efd48c386cdd3fd249e700db3923a6117163
emondewan00 <dewanmohammademon@gmail.com>
2024-08-25 15:09:44 +0600

    login form update with react form hook and show login status in notification

--- components/shared/LoginForm.jsx
+++ components/shared/LoginForm.jsx
@@ -1,12 +1,32 @@
+"use client";
+import { useForm } from "react-hook-form";
 import loginAction from "@/actions/login";
 import Link from "next/link";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const router = useRouter();
+  const onSubmit = async (data) => {
+    const result = await loginAction(data);
+    if (result.success) {
+      toast.success(result.message);
+      router.push("/");
+      router.refresh();
+    } else {
+      toast.error(result.message);
+    }
+  };
+
   return (
     <div className="self-center p-4 text-slate-700">
       <h1 className="text-4xl font-bold text-center">Login</h1>
-      {/* {err && <h3 className=" my-4 p-2 rounded bg-rose-600">{err} </h3>} */}
-      <form action={loginAction} className="space-y-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
         <div>
           <label htmlFor="email" className="text-xl">
             Email <span className="text-red-600">*</span>
@@ -14,10 +34,13 @@ const LoginForm = () => {
           <input
             type="email"
             id="email"
-            name="email"
-            className="w-full p-3  outline-blue-900 focus:outline duration-150 delay-100 ease-linear rounded mt-2"
+            {...register("email", { required: "Email is required" })}
+            className="w-full p-3 outline-blue-900 focus:outline duration-150 delay-100 ease-linear rounded mt-2"
             placeholder="example@gmail.com"
           />
+          {errors.email && (
+            <p className="text-red-600 mt-2">{errors.email.message}</p>
+          )}
         </div>
         <div>
           <label htmlFor="password" className="text-xl">
@@ -26,10 +49,13 @@ const LoginForm = () => {
           <input
             type="password"
             id="password"
-            name="password"
-            className="w-full p-3  outline-blue-900 focus:outline duration-150 delay-100 ease-linear rounded mt-2"
+            {...register("password", { required: "Password is required" })}
+            className="w-full p-3 outline-blue-900 focus:outline duration-150 delay-100 ease-linear rounded mt-2"
             placeholder="xxxxxxxx"
           />
+          {errors.password && (
+            <p className="text-red-600 mt-2">{errors.password.message}</p>
+          )}
         </div>
         <div className="flex justify-between items-center">
           <button
@@ -43,7 +69,7 @@ const LoginForm = () => {
           </p>
         </div>
         <p className="">
-          Not a member ?{" "}
+          Not a member?{" "}
           <Link href={"/register"} className="text-blue-600 underline">
             Register
           </Link>
